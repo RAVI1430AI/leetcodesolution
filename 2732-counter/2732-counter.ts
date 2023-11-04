40bf61a2b0b16b8aeb4aef17ae5b007a23276a38
RAVI KASAUDHAN <91520095+RAVI1430AI@users.noreply.github.com>
2023-11-04 17:52:18 +0530

    Time: 49 ms (80.9%), Space: 42.8 MB (58.11%) - LeetHub

---
+++ 2732-counter/2732-counter.ts
@@ -0,0 +1,24 @@
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
+
+
+
+
+ function createCounter(n: number): () => number {
+    let i = n;
+    return function () {
+        return i++;
+    };
+}
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
